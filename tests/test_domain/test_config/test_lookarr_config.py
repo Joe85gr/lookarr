fe396ef63919e231949c2bd147673c037394d0c2
Joe85gr <info@rainingdreams.co.uk>
2023-09-24 12:31:01 +0100

    updated how language is validated

--- tests/test_domain/test_config/test_lookarr_config.py
+++ tests/test_domain/test_config/test_lookarr_config.py
@@ -1,8 +1,4 @@
-import pytest
-from pydantic import ValidationError
-
 from src.domain.config.lookarr_config import LookarrConfig
-from src.constants import SUPPORTED_LANGUAGES
 
 
 class Test_LookarrConfig:
@@ -28,18 +24,3 @@ class Test_LookarrConfig:
 
         # Assert
         assert result.strict_mode is True
-
-    def test_invalid_language(self):
-        # Arrange
-        expectedErrorMessage = f"value must be one of: {''.join(SUPPORTED_LANGUAGES)}"
-
-        # Assert
-        with pytest.raises(ValidationError) as result:
-            LookarrConfig(
-                language="gibberish",
-                strict_mode_allowed_ids=[1, 2],
-                search_all_command="Search",
-            )
-
-        errorMessage = result.value.errors()[0]["msg"]
-        assert expectedErrorMessage in errorMessage
